a20a5c15f205c958e48d32bdb90018eaf04e2a56
QiuJieLi <liqiujie@cn.ibm.com>
2019-12-05 03:26:36 +0000

    first change

--- src/app/hello.go
+++ src/app/hello.go
@@ -14,7 +14,7 @@ func say(word string) string {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	log.Print("Say something!")
-	fmt.Fprintf(w, "%s\n", say("BLUE-1204!!!"))
+	fmt.Fprintf(w, "%s\n", say("BLUE-IBM!!!"))
 }
 
 func main() {
