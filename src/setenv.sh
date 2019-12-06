3e18aa912a1c4b5d8ab7103159262f6a2ab07042
QiuJieLi <liqiujie910@qq.com>
2019-12-06 14:21:46 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,4 +1,4 @@
-export GITACCOUNT=guoyingc
+export GITACCOUNT=
 
 export MYCLUSTER=
 export KUBECONFIG=
@@ -6,3 +6,4 @@ export KUBECONFIG=
 export REGISTRY=us.icr.io
 export NAMESPACE=tektondevops-<your_name>
 export EMAIL=
+export INGRESS=
